c15f613a65269ef3d029e1271ce18f5b2b44779a
tseomnidemo <19118354034@163.com>
2026-01-07 12:30:33 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -4,135 +4,137 @@ import { ElIcon } from 'element-plus'
 import { Document, Files, MagicStick, Picture, DataAnalysis, Film } from '@element-plus/icons-vue'
 
 // logo地址，没有则置为""即可
-const logo = './logo.png'
+const logo = './icon.png'
 
 // 标题
-const title = 'Academic Project Page Template'
+const title = 'TSE-Omni: Unify Audio and Visual Target Speech Extraction via a Single Autoregressive Large Language Model'
 
 // 标题颜色
 const title_color = '#000000'
 
 // 标题补充，没有则置为''即可
-const title_supp = ' (Vue based)'
+const title_supp = ''
 
 // 标题补充颜色
-const title_supp_color = '#42B883'
+const title_supp_color = '#43B883'
 
 // 按钮颜色
 const btn_color = '#444444'
 
-// 作者清单（包含作者姓名、头像、主页、地址序号）
-const authors = [
-  {
-    name: "Your Name",
-    icon: "./icon/junyaohu.jpg",
-    homepage: "https://junyaohu.github.io/",
-    address_flag: "1,#"
-  },
-  {
-    name: "Anya Forger",
-    icon: "./icon/anya.jpg",
-    homepage: "https://www.bilibili.com/video/BV1jv4y1P7Bb",
-    address_flag: "2,#"
-  },
-  {
-    name: "BugCat Capoo",
-    icon: "./icon/capoo.webp",
-    homepage: "https://zh.moegirl.org.cn/%E7%8C%AB%E7%8C%AB%E8%99%AB%E5%92%96%E6%B3%A2",
-    address_flag: "1,*"
-  },
-]
+// // 作者清单（包含作者姓名、头像、主页、地址序号）
+// const authors = [
+//   {
+//     name: "Wenxuan Wu",
+//     homepage: "https://alexwxwu.github.io/",
+//     address_flag: "1"
+//   },
+//   {
+//     name: "Shuai Wang",
+//     homepage: "https://shuaiwang-nju.github.io/",
+//     address_flag: "2,"
+//   },
+//   {
+//     name: "Xixin Wu",
+//     homepage: "https://www1.se.cuhk.edu.hk/~wuxx/",
+//     address_flag: "1"
+//   },
+// 	  {
+//     name: "Helen Meng",
+//     address_flag: "1"
+//   },
+// 	  {
+//     name: "Haizhou Li",
+//     address_flag: "3"
+//   },
+// ]
 
-// 地址清单（包含地址名称、头像、主页、地址序号）
-const addresses = [
-  {
-    address_flag: "1",
-    name: "Home University",
-    icon: "./icon/home.png",
-    homepage: "https://github.com/hmuniversity"
-  },
-  {
-    address_flag: "2",
-    name: "IKUN University",
-    icon: "./icon/ikun.avif",
-    homepage: "https://www.bilibili.com/video/BV178411Y7QB"
-  },
-]
+// // 地址清单（包含地址名称、头像、主页、地址序号）
+// const addresses = [
+//   {
+//     address_flag: "1",
+//     name: "CUHK",
+//   },
+//   {
+//     address_flag: "2",
+//     name: "NJU",
+//   },
+// 	{
+//     address_flag: "3",
+//     name: "CUHK-SZ",
+//   },
+// ]
 
-// 共一和通讯提示
-const con_and_corresponding_author = 
-  "#: Equal Contribution. *: Corresponding Author."
+// // 共一和通讯提示
+// const con_and_corresponding_author = 
+//   "*: Corresponding Author."
 
 // 最新消息
-const news = "🔥 [2024-12-15] This template project is still under development."
+//const news = "🔥 [2024-12-15] This template project is still under development."
 
 // 强调内容
 const emphases = [
-  "🎉 [ABCD 2024] Poster",
-  "🥰 欢迎关注“减论”微信公众号/B站/知乎/小红书",
-  "传递人工智能算法科普教育的减约理解",
-  "提升信息效率及认知维度"
+	"Efficient: A unified framework that supports both audio-based and visual-based TSE within a single autoregressive LLM, eliminating the need for modality-specific architectures and reducing deployment complexity.",
+	"Cognitive-inspired: Leverages the LLM's next-token prediction capability to naturally integrate historical predicted speech semantic tokens as self-enrollment, seamlessly align them with synchronous visual semantic tokens, emulating human multisensory integration.",
+	"Support target speaker switch, visual corruption, and streaming inference scenario."
+  // "🎉 [ABCD 2024] Poster",
+  // "🥰 欢迎关注“减论”微信公众号/B站/知乎/小红书",
+  // "传递人工智能算法科普教育的减约理解",
+  // "提升信息效率及认知维度"
 ]
 
 // 提供引导资料链接
-const buttons = [
-  {
-    disabled: true,
-    name: "Paper",
-    component: Document,
-  },
-  {
-    disabled: true,
-    name: "中译版",
-    component: Document,
-  },
-  {
-    disabled: false,
-    name: "Code",
-    link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
-    component: Files,
-  },
-  {
-    disabled: false,
-    name: "Demo",
-    link: "https://junyaohu.github.io/academic-project-page-template-vue",
-    component: MagicStick,
-  },
-  {
-    disabled: true,
-    name: "Poster",
-    component: Picture,
-  },
-  {
-    disabled: true,
-    name: "Slide",
-    component: DataAnalysis,
-  },
-  {
-    disabled: false,
-    name: "Video (减论)",
-    link: "https://www.bilibili.com/video/BV15XkgYiE73/",
-    component: Film,
-  },
-  {
-    disabled: false,
-    name: "Video (Tutorial)",
-    link: "https://www.bilibili.com/video/BV1oUrfYzEqZ",
-    component: Film,
-  },
-]
+// const buttons = [
+ //  {
+ //    disabled: false,
+ //    name: "Paper",
+ //   link: "https://arxiv.org/abs/2511.06288",
+	// component: Document,
+ //  },
+  // {
+  //   disabled: true,
+  //   name: "中译版",
+  //   component: Document,
+  // },
+  // {
+  //   disabled: false,
+  //   name: "Code",
+  //   link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
+  //   component: Files,
+  // },
+  // {
+  //   disabled: false,
+  //   name: "Demo",
+  //   link: "https://junyaohu.github.io/academic-project-page-template-vue",
+  //   component: MagicStick,
+  // },
+  // {
+  //   disabled: true,
+  //   name: "Poster",
+  //   component: Picture,
+  // },
+  // {
+  //   disabled: true,
+  //   name: "Slide",
+  //   component: DataAnalysis,
+  // },
+  // {
+  //   disabled: false,
+  //   name: "Video (减论)",
+  //   link: "https://www.bilibili.com/video/BV15XkgYiE73/",
+  //   component: Film,
+  // },
+  // {
+  //   disabled: false,
+  //   name: "Video (Tutorial)",
+  //   link: "https://www.bilibili.com/video/BV1oUrfYzEqZ",
+  //   component: Film,
+  // },
+// ]
 
 </script>
 
 <template>
   <div>
-
-    <!-- 最新消息提示 -->
-    <el-row justify="center">
-      <el-col :span="24">
-        <el-alert title="🔥 This template is still under development." type="success" />
-      </el-col>
-    </el-row>
 
     <!-- 文章logo -->
     <el-row v-if="logo" justify="center">
@@ -161,8 +163,8 @@ const buttons = [
       </a>
     </el-row>
 
-    <!-- 地址名单 -->
-    <el-row justify="center">
+   <!-- 地址名单 -->
+ <el-row justify="center">
       <a :href=address.homepage v-for="address in addresses">
         <el-button class="title-button" type="primary" text>
           <el-avatar v-if="address.icon" :size="40" :src="address.icon" />
@@ -183,7 +185,7 @@ const buttons = [
         {{ emphasis }}
     </el-row>
 
-    <!-- 提供引导按钮 -->
+	<!-- 提供引导按钮 -->
     <el-row justify="center" style="margin-bottom: 20px;">
       <el-col :span="20">
         <el-row justify="center">
@@ -199,6 +201,7 @@ const buttons = [
       </el-col>
     </el-row>
 
+ 
   </div>
 </template>
 
@@ -289,10 +292,18 @@ const buttons = [
   margin: 0;
 }
 
-.logo {
-  width: 150px; 
-  height: 150px;
+/* .logo {
+  width: 200px; 
+  height: 180px;
   border-radius: 50%;
+  box-shadow: #ced3dc 0px 0px 3px 2px;
+  margin-top: 40px;
+} */
+
+.logo {
+  width: 240px; 
+  height: 240px; /* 成为正方形 */
+  /* border-radius: 50%;  可以注释掉或删除 */
   box-shadow: #ced3dc 0px 0px 3px 2px;
   margin-top: 40px;
 }
